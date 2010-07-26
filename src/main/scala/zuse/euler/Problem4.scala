0ea2db9043be6eddbbde4d02e36364fa6abe085c
retronym <jzaugg@gmail.com>
2010-07-26 22:08:58 +0200

    Project Euler 4

---
+++ src/main/scala/zuse/euler/Problem4.scala
@@ -0,0 +1,17 @@
+package zuse.euler
+
+/**
+ * http://projecteuler.net/index.php?section=problems&id=4
+ *
+ * A palindromic number reads the same both ways. The largest palindrome made from the product
+ * of two 2-digit numbers is 9009 = 91  99.
+ *
+ * Find the largest palindrome made from the product of two 3-digit numbers.
+ */
+object Problem4 {
+  case class Result(x: Int, y: Int, product: Int)
+
+  def largestPalindrome: Result = {
+    error("TODO")
+  }
+}
