6b931982736fa75f6d598ec12f21a974ecc10b57
retronym <jzaugg@gmail.com>
2010-07-26 22:40:57 +0200

    Add dependency on Scalaz

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -1,5 +1,6 @@
 import sbt._
 
 class Project(info: ProjectInfo) extends DefaultProject(info) {
-  val specsDependency = "org.scala-tools.testing" %% "specs" % "1.6.5" % "test" withSources
+  val specs = "org.scala-tools.testing" %% "specs" % "1.6.5" % "test" withSources
+  val scalaz = "com.googlecode.scalaz" %% "scalaz-core" % "5.0" withSources
 }
