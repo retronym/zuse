1a5fd10e954a23b16e0ce664aa3a1fe727e852c7
retronym <jzaugg@gmail.com>
2010-07-26 22:21:12 +0200

    Project Euler 4

--- src/main/scala/zuse/euler/Problem4.scala
+++ src/main/scala/zuse/euler/Problem4.scala
@@ -8,10 +8,19 @@ package zuse.euler
  *
  * Find the largest palindrome made from the product of two 3-digit numbers.
  */
-object Problem4 {
+object Problem4 extends Application {
   case class Result(x: Int, y: Int, product: Int)
 
   def largestPalindrome: Result = {
-    error("TODO")
+    val palindromes = for{
+      i <- (1 to 999)
+      j <- (1 to i)
+      product = i * j
+      if product.toString == product.toString.reverse
+    } yield Result(i, j, product)
+
+    palindromes.max(Ordering.by((r: Result) => r.product))
   }
+
+  assert(largestPalindrome == Result(993, 913, 906609))
 }
