20a2960e54fe6264e520ccaaa13bb384e31c1709
retronym <jzaugg@gmail.com>
2010-07-26 22:51:54 +0200

    Euler 1

--- src/main/scala/zuse/euler/Problem1.scala
+++ src/main/scala/zuse/euler/Problem1.scala
@@ -9,11 +9,24 @@ package zuse.euler
  * Find the sum of all the multiples of 3 or 5 below 1000.
  */
 object Problem1 extends Application {
+  def multipleOf(n: Int)(x: Int) = x % n == 0
+
   def sumMultiples: Int = {
-    def multipleOf(n: Int)(x: Int) = x % n == 0
     val ms = (1 to 1000).filter(x => multipleOf(3)(x) || multipleOf(5)(x))
     ms.sum
   }
 
+  def sumMultiplesScalaz: Int = {
+    import scalaz._
+    import Scalaz._
+
+    def multipleOf3Or5 = Seq(3, 5) ∘ (n => multipleOf(n) _) ∑
+
+    val ms = (1 to 1000) filter multipleOf3Or5
+    ms.sum
+  }
+
   assert(sumMultiples == 234168)
+
+  assert(sumMultiplesScalaz == 234168)
 }
